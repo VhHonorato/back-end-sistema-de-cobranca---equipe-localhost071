7cfdae0789982ca7696b9dd75b6e5b1e69fefc95
vhhonorato <vhhonorato@hotmail.com>
2021-10-01 13:58:18 -0300

    fix: ajuste para criação do banco de dados na heroku.

--- src/conexao.js
+++ src/conexao.js
@@ -1,10 +1,21 @@
+// const knex = require('knex')({
+//     client: 'pg',
+//     connection: {
+//         host: 'localhost',
+//         user: 'postgres',
+//         password: 'jsm1183',
+//         database: 'sistema_cobranca_localhost071'
+//     }
+// });
+
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        host: 'localhost',
-        user: 'postgres',
-        password: 'jsm1183',
-        database: 'sistema_cobranca_localhost071'
+        host : 'ec2-107-22-18-26.compute-1.amazonaws.com',
+        port : 5432,
+        user : 'rcthgtcijgsglm',
+        password : '814e04ce09bbccb47135938c3499047924af77928173e2fc500322c03e032723',
+        database : 'devek15gco18hq'
     }
 });
 
