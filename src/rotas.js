e79c50a0f0026627bfa01434a466e53ff16583c7
Pedro Gomes <pgsantanadesouza@usp.br>
2021-10-02 18:08:12 -0300

    Corrigido erro na rota de cadastrar cliente

--- src/rotas.js
+++ src/rotas.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const cadastrarUsuario = require('./controladores/cadastroUsuario');
-const loginUsuario = require('./controladores/loginUsuario');
-const cadastrarCliente = require('./controladores/cadastroCliente');
-const verificaLogin = require('./filtros/verificaLogin')
+const express = require("express");
+const cadastrarUsuario = require("./controladores/cadastroUsuario");
+const loginUsuario = require("./controladores/loginUsuario");
+const cadastrarCliente = require("./controladores/cadastroCliente");
+const verificaLogin = require("./filtros/verificaLogin");
 const rotas = express();
 
-rotas.post('/cadastro', cadastrarUsuario.cadastrarUsuario);
+rotas.post("/cadastro", cadastrarUsuario.cadastrarUsuario);
 
-rotas.post('/login', loginUsuario.loginUsuario);
+rotas.post("/login", loginUsuario.loginUsuario);
 
 rotas.use(verificaLogin);
 
-rotas.get('/cadastro', cadastrarUsuario.exibirCadastro);
-rotas.put('/cadastro', cadastrarUsuario.atualizarCadastro);
-rotas.get('/usuario/cadastro', cadastrarCliente.cadastrarCliente);
+rotas.get("/cadastro", cadastrarUsuario.exibirCadastro);
+rotas.put("/cadastro", cadastrarUsuario.atualizarCadastro);
+rotas.post("/usuario/cadastro", cadastrarCliente.cadastrarCliente);
 module.exports = rotas;
