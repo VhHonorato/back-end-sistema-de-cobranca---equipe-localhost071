9202a511ce24e8158f6c72bacbe02c2046d5ad4e
Pedro Gomes <pgsantanadesouza@usp.br>
2021-10-11 16:19:25 -0300

    fix: consertando erro de digitacao na rota de listar cobrancas

--- src/controladores/cadastroCobranca.js
+++ src/controladores/cadastroCobranca.js
@@ -1,65 +1,67 @@
-const knex = require('../conexao');
-const {cadastroCobrancaSchema} = require('../validacoes/cadastroSchema');
-
+const knex = require("../conexao");
+const { cadastroCobrancaSchema } = require("../validacoes/cadastroSchema");
 
 const cadastrarCobranca = async (req, res) => {
-    const {
+  const { descricao, status, valor, vencimento } = req.body;
+  const { id } = req.params;
+  try {
+    await cadastroCobrancaSchema.validate(req.body);
+
+    const selecionarCliente = await knex("clientes").where({ id }).first();
+
+    if (!selecionarCliente) {
+      return res
+        .status(404)
+        .json(
+          "Não foi possível selecionar o cliente para realizar o cadastro da cobrança. Verifique as credênciais do cliente"
+        );
+    }
+
+    const cadastrandoCobranca = {
+      cliente_id: selecionarCliente.id,
       descricao,
       status,
       valor,
       vencimento,
-    } = req.body;
-    const {id} = req.params;
-    try {
-       
-        await cadastroCobrancaSchema.validate(req.body);
+    };
+    const queryInserirCobranca = await knex("cobrancas").insert(
+      cadastrandoCobranca
+    );
 
-        const selecionarCliente = await knex('clientes').where({id}).first();
-     
-        if(!selecionarCliente){
-            return res.status(404).json('Não foi possível selecionar o cliente para realizar o cadastro da cobrança. Verifique as credênciais do cliente');
-        }
-     
-        const cadastrandoCobranca = {
-            cliente_id: selecionarCliente.id,
-            descricao,
-            status,
-            valor,
-            vencimento  
-        };
-        const queryInserirCobranca = await knex('cobrancas').insert(cadastrandoCobranca);
-
-        if(!queryInserirCobranca){
-            return res.status(400).json('Desculpe, não foi possível cadastrar a cobrança.')
-        }
-
-        return res.status(200).json('Cobranca cadastrada com sucesso.')
-    } catch (error) {
-        return res.status(400).json(error.message);
+    if (!queryInserirCobranca) {
+      return res
+        .status(400)
+        .json("Desculpe, não foi possível cadastrar a cobrança.");
     }
 
-}
+    return res.status(200).json("Cobranca cadastrada com sucesso.");
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+};
 
 const listarCobranca = async (req, res) => {
-    const {id} = req.usuario;
-    try {
-       
-        const listaDeCobrancas = await knex.select('*').from('clientes')
-        .fullOuterJoin('cobrncas', 'cobranca.cliente_id', 'cliente.id')
-        .where({usuario_id: id});
+  const { id } = req.usuario;
+  try {
+    const listaDeCobrancas = await knex
+      .select("*")
+      .from("clientes")
+      .fullOuterJoin("cobrancas", "cobranca.cliente_id", "cliente.id")
+      .where({ usuario_id: id });
 
-        if(!listaDeCobrancas){
-           return res.status(400).json('Não foi possivel listar as cobranças dos clientes');
-        }
-
-        return res.status(200).json(listaDeCobrancas);
-    } catch (error) {
-        return res.status(400).json(error.message);
+    if (!listaDeCobrancas) {
+      return res
+        .status(400)
+        .json("Não foi possivel listar as cobranças dos clientes");
     }
-    
-}
+
+    return res.status(200).json(listaDeCobrancas);
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+};
 
 module.exports = {
-    cadastrarCobranca,
-    listarCobranca
-}
+  cadastrarCobranca,
+  listarCobranca,
+};
